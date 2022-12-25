57d570e7889dd2437df5130a832e549eeb18e1d0
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-12-26 00:10:50 +1100

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error', err)
+  process.exit(1) //mandatory (as per the Node.js docs)
+})
+
+export const handler = (): string => {
+  console.log('hello')
+  return 'hello'
+}
+
+handler()
