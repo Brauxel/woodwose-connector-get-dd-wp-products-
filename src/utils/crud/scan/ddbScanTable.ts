a1fbf837e28f1fd8aa5078fee22a800d9d97c1cd
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-12-28 21:07:21 +1100

    successfully created id based query

--- src/utils/crud/scan/ddbScanTable.ts
+++ src/utils/crud/scan/ddbScanTable.ts
@@ -1,23 +1,26 @@
-import { QueryInput } from '@aws-sdk/client-dynamodb'
+import { ScanInput } from '@aws-sdk/client-dynamodb'
 import { ScanCommand } from '@aws-sdk/lib-dynamodb'
 import { ddbDocClient } from '../../../libs/ddbDocClient'
 import { DynamoDbWordPressProduct } from '../../../types/dataTypes'
 import { logAndThrowError } from '../../logger/loggerHelpers'
 
-export const scanTableInDynamoDb = async (tableName: string) => {
+export const scanProductsTableInDynamoDb = async () => {
   try {
-    const params: QueryInput = {
-      TableName: tableName,
+    const params: ScanInput = {
+      TableName: process.env.WORDPRESS_PRODUCTS_TABLE_NAME,
     }
     const data = await ddbDocClient.send(new ScanCommand(params))
 
     return data.Items as DynamoDbWordPressProduct[]
   } catch (err) {
     const error = err as Error
-    return logAndThrowError(`Error in Scan Table: ${tableName}`, {
-      name: error.name,
-      message: error.message,
-      stack: error.stack,
-    })
+    return logAndThrowError(
+      `Error in Scan Table: ${process.env.WORDPRESS_PRODUCTS_TABLE_NAME}`,
+      {
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      }
+    )
   }
 }
